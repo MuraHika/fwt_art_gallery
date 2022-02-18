6c52a58effd69180527159fd804ccc11955920f3
MuraHika <kima.bright@mail.ru>
2022-02-18 14:59:35 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-     fffffffffffffffffff
-    </div>
-  );
+  return <div>fffffffffffffffffff</div>;
 }
 
 export default App;
