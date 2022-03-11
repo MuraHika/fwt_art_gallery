81c86c860cf2ea3f35830211ca29c9560c4fc4aa
o.sazhnikova <o.sazhnikova@framework.team>
2022-03-11 15:08:45 +0400

    create accordion component

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import Arrow from "../../assets/Arrow.svg";
+import ResizeScreen from "../../utils/ScreenSize";
+import "./styles.scss";
+
+interface AccordionProps {
+  theme?: "dark" | "light";
+  text: string;
+}
+
+function Accordion({ theme, text } : AccordionProps) {
+  const [buttonText, setButtonText] = useState<string>("Show all"); 
+  
+  const [showText, setShowText] = useState<string>(""); 
+  const [hideText, setHideText] = useState<string>(""); 
+  const screen = ResizeScreen();
+  const [isHide, setIsHide] = useState<boolean>(true); 
+
+  useEffect(() => {
+    if (text.length >= 238) {
+      for (let index = 0; index < text.length; index += 1) {
+        if (index <= 238) {
+          setShowText((prevState) => (prevState + text[index]));
+        } else {
+
+          setHideText((prevState) => (prevState + text[index]));
+        }
+      }
+      console.log(text.length);
+    }
+  }, []);
+
+  const SetHide = () => {
+    const nowIsHide = !isHide;
+
+    setShowText((prevState) => ( nowIsHide ? prevState.replace(hideText, "") : prevState + hideText));
+    setButtonText(nowIsHide ? "Show all" : "Show less");
+    setIsHide(nowIsHide);
+  };
+
+  return (
+  <div className={`accordion accordion--${theme}`}>
+    <div className={`accordion-text ${isHide ? "accordion-text--hide" : "accordion-text--show"}`}>
+      <p className={`${isHide ? 'show-text' : ''}`}>
+        <span className='text'>{showText}</span>
+        {screen.isMobile && 
+          <span className={`accordion-button_hide ${isHide ? "arrow--hide" : "arrow--show"}`} onClick={() => SetHide()} >
+            <Arrow />
+          </span> 
+        }
+      </p>
+      
+    </div>
+    {!screen.isMobile && <div className={`accordion-button_hide ${isHide ? "arrow--hide" : "arrow--show"}`} onClick={() => SetHide()}>
+      <span>{buttonText}</span>
+      <Arrow />
+    </div>}
+  </div>
+  );
+}
+
+export default Accordion;
+
+Accordion.defaultProps = {
+  theme: "light",
+};
--- src/components/ArtistHeader/ArtistHeader.tsx
+++ src/components/ArtistHeader/ArtistHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./styles.scss";
 import Tag from '../Tag';
+import Accordion from '../Accordion';
 import ArtistAva from "../../assets/artist_ava.png";
 
 interface ArtistHeaderProps {
@@ -10,9 +11,10 @@ interface ArtistHeaderProps {
 function ArtistHeader({ theme } : ArtistHeaderProps) {
 
   const genres = ["anime & manga", "architectural", "maya", "realistic", "Unreal engine", "weapons"];
+  const text = "He studied in the Russian Empire and the United States, but spent most of his active life in England. He is best known for his portraits of his contemporaries. Experienced the influence of realists in the person of his friend Gustave Courbet and the Pre-Raphaelites, as well as Japanese art. In a number of creative methods it was close to impressionism.";
 
   return (
-    // <div style={{ width: "100%" }}>
+  // <div style={{ width: "100%" }}>
       <div className={`artist-container artist-container--${theme}`}>
         <div className="artist-profile">
           <img className='artist_profile-picture' src={`${ArtistAva}`} alt="author"/>
@@ -21,12 +23,7 @@ function ArtistHeader({ theme } : ArtistHeaderProps) {
           <span className='artist_profile-year'>July 11, 1834 - July 17, 1903</span>
         </div>
         <div className={`artist-info artist-info--${theme}`}>
-          <span className='artist_info-bio'>
-            He studied in the Russian Empire and the 
-            United States, but spent most of his active life in England. He is best known for his portraits of his contemporaries.
-            Experienced the influence of realists in the person of his friend Gustave Courbet and the Pre-Raphaelites, as well as 
-            Japanese art. In a number of creative methods it was close to impressionism.
-          </span>
+          <Accordion theme={theme} text={text} />
           <span className='artist_info-place'>London, Great Britain</span>
           <div className='artist-tags'>
             {genres.map((el) => (
@@ -35,7 +32,7 @@ function ArtistHeader({ theme } : ArtistHeaderProps) {
           </div>
         </div>
       </div>
-    // </div>
+  // </div>
   );
 }
 
